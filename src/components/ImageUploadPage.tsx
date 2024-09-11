d7e1d18a5bf4974cacf0600e525d14ebe640d2fd
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2024-09-11 12:18:54 +0300

    created basics for ImageUploadPage

---
+++ src/components/ImageUploadPage.tsx
@@ -0,0 +1,19 @@
+import '../App.css';
+
+const ImageUploadPage = () => {
+
+    return (
+        <>
+            <h1 className='h1'>Lataa kuva</h1>
+
+            <p className="text">Varmista, että kaluste on hyvin valaistu ja koko huonekalu näkyy kuvassa.</p>
+
+            <button className='button'>OTA KUVA</button>
+
+            <button className='button'>GALLERIA</button>
+        </>
+    )
+}
+
+export default ImageUploadPage;
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import ImageUploadPage from './components/ImageUploadPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ImageUploadPage />
   </StrictMode>,
 )
