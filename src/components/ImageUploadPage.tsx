709bfb6ff3d8b4d9b66767c24f30ae00b8cf463d
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2024-09-16 15:03:20 +0300

    added basic functionality for camera

--- src/components/ImageUploadPage.tsx
+++ src/components/ImageUploadPage.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import {Camera} from "react-camera-pro";
 import '../App.css';
 
 const ImageUploadPage = () => {
+    const camera = useRef(null);
+    const [image, setImage] = useState(null);
+    const [takeImage, setTakeImage] = useState(false);
 
     return (
         <>
@@ -9,8 +13,34 @@ const ImageUploadPage = () => {
 
             <p className="text">Varmista, että kaluste on hyvin valaistu ja koko huonekalu näkyy kuvassa.</p>
 
-            <button className='button'>OTA KUVA</button>
+            <button className='button' onClick={() => setTakeImage(true)}>OTA KUVA</button>
 
+            {takeImage &&
+            <div>
+                    <button className='button' onClick={() => setImage(camera.current.takePhoto())}>
+                        Ota kuva
+                    </button>
+
+                    <button className='button' onClick={() => setTakeImage(false)}>
+                        Sulje kamera
+                    </button>
+            </div>
+}
+
+            {takeImage &&
+            <div className="camera-container">
+                <Camera ref={camera} />
+            </div>
+            }
+                
+                {image && (
+                    <div>
+                        <h2>Kuva otettu:</h2>
+                        <img src={image} alt='Taken' />
+                        <button onClick={() => setImage(null)}>POISTA KUVA</button>
+                    </div>
+                )}   
+            
             <button className='button'>GALLERIA</button>
         </>
     )
