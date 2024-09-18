165ab576c539f7bf37da1becae96c82602768150
Lassive <lassive9@gmail.com>
2024-09-18 09:32:30 +0300

    Welcome page basis created

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import WelcomePage from './components/WelcomePage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WelcomePage />
   </StrictMode>,
 )
