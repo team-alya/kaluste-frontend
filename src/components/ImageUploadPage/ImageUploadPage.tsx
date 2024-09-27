345287400967c95ca4124edf43a3eb34d6e64a65
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2024-09-27 09:47:46 +0300

    conditional rendering shows ImageConfirmationPage

--- src/components/ImageUploadPage/ImageUploadPage.tsx
+++ src/components/ImageUploadPage/ImageUploadPage.tsx
@@ -19,68 +19,71 @@ const ImageUploadPage = () => {
 
     return (
         <div className="container">
-            <h1 className='h1'>Lataa kuva</h1>
-
-            <p className="text">Varmista, että kaluste on hyvin valaistu ja koko huonekalu näkyy kuvassa.</p>
-
-            {!takeImage && (
+            {!image ? (
                 <>
-                <div className="button-container">
-                    <button className='button' onClick={() => setTakeImage(true)}>
-                        OTA KUVA
-                    </button>
-                    
-                    <button className="button" onClick={handleFileInputClick}>
-                        GALLERIA
-                    </button>
-                    
-                    {/* Hidden file input */}
-                    <input
-                        type="file"
-                        id="file-input"
-                        className="file-input"
-                        ref={fileInputRef}
-                        onChange={handleChange}
-                        style={{ display: 'none' }} // Hide the input element
-                    />
+                    <h1 className='h1'>Lataa kuva</h1>
+
+                    <p className="text">Varmista, että kaluste on hyvin valaistu ja koko huonekalu näkyy kuvassa.</p>
+
+                    {!takeImage ? (
+                        <div className="button-container">
+                            <button className='button' onClick={() => setTakeImage(true)}>
+                                OTA KUVA
+                            </button>
+
+                            <button className="button" onClick={handleFileInputClick}>
+                                GALLERIA
+                            </button>
+
+                            {/* Hidden file input */}
+                            <input
+                                type="file"
+                                id="file-input"
+                                className="file-input"
+                                ref={fileInputRef}
+                                onChange={handleChange}
+                                style={{ display: 'none' }} // Hide the input element
+                            />
+                        </div>
+                    ) : (
+                        <>
+                            <button className='button' onClick={() => {
+                                setImage(camera.current.takePhoto());
+                                setTakeImage(false);
+                            }}>
+                                Ota kuva
+                            </button>
+                            <button className='button' onClick={() => setTakeImage(false)}>
+                                Sulje kamera
+                            </button>
+
+                            <div className="camera-container">
+                                <Camera
+                                    ref={camera}
+                                    facingMode="environment"
+                                    aspectRatio={4 / 3}
+                                    errorMessages={{
+                                        noCameraAccessible: undefined,
+                                        permissionDenied: undefined,
+                                        switchCamera: undefined,
+                                        canvas: undefined
+                                    }}
+                                />
+                            </div>
+                        </>
+                    )}
+                </>
+            ) : (
+                <div className="image-container">
+                    <h1 className="h1">Kalusteen tunnistus</h1>
+                    <p className="text">Onko kuvassa kalusteesi?</p>
+                    <img src={image} alt='Taken' />
+                    <div className="button-container">
+                        <button className="button">KYLLÄ</button>
+                        <button className="button" onClick={() => setImage(null)}>EI</button>
+                    </div>
                 </div>
-            </>
-            
             )}
-            {takeImage && (
-                <>
-                <button className='button' onClick={() => {
-                    setImage(camera.current.takePhoto());
-                    setTakeImage(false);
-                }}>
-                    Ota kuva
-                </button>
-                <button className='button' onClick={() => setTakeImage(false)}>
-                    Sulje kamera
-                </button>
-
-                    <div className="camera-container">
-                        <Camera 
-                            ref={camera}
-                            facingMode="environment"
-                            aspectRatio={4 / 3} errorMessages={{
-                                noCameraAccessible: undefined,
-                                permissionDenied: undefined,
-                                switchCamera: undefined,
-                                canvas: undefined
-                            }}                        />
-                        
-                    </div>
-
-            </>
-            )}
-                
-                {image && (
-                    <div className="image-container">
-                        <img src={image} alt='Taken' />
-                        <button className="button" onClick={() => setImage(null)}>POISTA KUVA</button>
-                    </div>
-                )}   
         </div>
     );
 }
