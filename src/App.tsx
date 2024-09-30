064db5cab878ee25847e622c65363bfb15946ee4
Lassive <lassive9@gmail.com>
2024-09-30 14:14:36 +0300

    Added navigation basis

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Router components
 import FurniConfirmPage from "./components/ConfirmationPage/ConfirmationPage";
 import ImageUploadPage from "./components/ImageUploadPage/ImageUploadPage";
-import WelcomePage from "./components/WelcomePage";
+import WelcomePage from "./components/WelcomePage/WelcomePage";
+import { Button } from "@mui/material";
+import "./App.css";
 
 function App() {
-  return <WelcomePage />;
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  return (
+    <Router>
+      <div className={darkTheme ? "app dark-theme" : "app light-theme"}>
+        <Button
+          variant="contained"
+          color="primary"
+          className="theme-toggle-button"
+          onClick={toggleTheme}
+        >
+          Vaihda Teemaa
+        </Button>
+
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<WelcomePage />} /> {/* Home route */}
+          <Route path="/upload" element={<ImageUploadPage />} />{" "}
+          <Route path="/confirmation" element={<FurniConfirmPage />} />{" "}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
